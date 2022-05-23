969c8340313f654c984d7caf032b8cf614f9c6ab
Hoffmann-Ferreira <andreihf@outlook.com>
2022-05-23 14:57:29 -0300

    feat: Botões de pesquisa, Delete e atualizar.

--- assets/script.js
+++ assets/script.js
@@ -1,141 +1,266 @@
+//  async function listarTodasAsPaletas() {
+//     const response = await fetch("http://localhost:3005/paletas/listar-todas/");
 
- async function listarTodasAsPaletas() {
-    const response = await fetch("http://localhost:3005/paletas/listar-todas/");
-    
-    const paletas = await response.json();
+//     const paletas = await response.json();
 
-    paletas.forEach(function (element) {
-      
-      document.querySelector("#paletaList").insertAdjacentHTML(
-          "beforeend",
-           `<div class= "paletaListaItem">
-                <div>
-                   <div class ="PaletaListaItem__sabor"> ${element.sabor} </div>
-                   <div class ="PaletaListaItem__preco"> R$ ${parseFloat(element.preco).toFixed(2)} </div>
-                   <div class ="PaletaListaItem__descricao"> ${element.descricao} </div>
-                </div>
-                <img class ="PaletaListaItem__foto" src=${element.foto} alt=${element.sabor}/>
-           </div>`
-       );
-   });
-};
+//     paletas.forEach(function (element) {
 
-listarTodasAsPaletas();
+//       document.querySelector("#paletaList").insertAdjacentHTML(
+//           "beforeend",
+//            `<div class= "paletaListaItem">
+//                 <div>
+//                    <div class ="PaletaListaItem__sabor"> ${element.sabor} </div>
+//                    <div class ="PaletaListaItem__preco"> R$ ${parseFloat(element.preco).toFixed(2)} </div>
+//                    <div class ="PaletaListaItem__descricao"> ${element.descricao} </div>
+//                 </div>
+//                 <img class ="PaletaListaItem__foto" src=${element.foto} alt=${element.sabor}/>
+//            </div>`
+//        );
+//    });
+// };
+
+// listarTodasAsPaletas();
 
 //variáveis auxiliares
 
-const baseUrl = "http://localhost:3005"
+const baseUrl = "http://localhost:3005";
 
-//requisições: 
+//requisições:
 //Buscar todas as paletas;
 
 const buscarTodasAsPaletas = async () => {
-   const resposta = await fetch(`${baseUrl}/paletas/listar-todas/`);
+  const resposta = await fetch(`${baseUrl}/paletas/listar-todas/`);
 
-   const paletas = await resposta.json();
+  const paletas = await resposta.json();
 
-   console.log(paletas);
-   
-   return paletas;
+  return paletas;
 };
-
-// buscarTodasAsPaletas();
 
 //Buscar por Id
 
 const buscarPaletasPorId = async (id) => {
-   const resposta = await fetch(`${baseUrl}/paletas/paleta/${id}`);
+  const resposta = await fetch(`${baseUrl}/paletas/paleta/${id}`);
 
-   const paleta = await resposta.json();
+  const paleta = await resposta.json();
 
-   console.log(paleta);
-
-   return paleta;
+  return paleta;
 };
-
-// buscarPaletasPorId(1);
 
 //Criar Paleta;
 
 const CriarPaleta = async (sabor, descricao, foto, preco) => {
-   const paleta = {
-      sabor,
-      descricao,
-      foto,
-      preco,
-   };
+  const paleta = {
+    sabor,
+    descricao,
+    foto,
+    preco,
+  };
 
-   const resposta = await fetch(`${baseUrl}/paletas/criar-paleta`, {
-      method: "POST",
-      headers: {
-         "Content-type": "application/json"
+  const resposta = await fetch(`${baseUrl}/paletas/criar-paleta`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(paleta),
+  });
 
-      },
-      mode:"cors",
-      body: JSON.stringify(paleta),
-   });
+  const novaPaleta = await resposta.json();
 
-   const novaPaleta = await resposta.json();
-   console.log(novaPaleta);
-
-   return novaPaleta;
-
+  return novaPaleta;
 };
+// funcao que vai renderizar na tela o formulário para a pessoa poder digitar novamente os dados
 
-// CriarPaleta("chocolate", "teste","testefoto", "10.50" );
+const formularioAtualizar = async (id) => {
+  document.querySelector("#atualizar").innerHTML = `
+   <div>
+      <form>
+         <feldset>
+            <legend>Atualizar Paleta</legend>
+
+            <label for= "sabor">Sabor:</label>
+            <input type= "text" id="sabor" name= "sabor" placeholder = "sabor" required/>
+
+            <label for= "descricao">Descrição:</label>
+            <input type= "text" id="descricao" name= "descricao" required/>
+
+            <label for= "foto">Foto:</label>
+            <input type= "url" id="foto" name= "foto"/>
+
+            <label for= "preco">Preço:</label>
+            <input type= "number" id="preco" name= "preco" required/>
+
+         </feldset>
+         <button onclick="atualizarPaleta(${id})">Atualizar</button>
+      </form>
+
+   </div>`;
+
+  const paletaFiltro = await fetch(`${baseUrl}/paletas/paleta/${id}`);
+
+  const paleta = await paletaFiltro.json();
+  paleta.length != 0
+    ? ((document.querySelector("#sabor").value = paleta.sabor),
+      (document.querySelector("#descricao").value = paleta.descricao),
+      (document.querySelector("#foto").value = paleta.foto),
+      (document.querySelector("#preco").value = paleta.preco))
+    : ((document.querySelector("#sabor").value = ""),
+      (document.querySelector("#descricao").value = ""),
+      (document.querySelector("#foto").value = ""),
+      (document.querySelector("#preco").value = 0));
+};
 
 //Atualizar paleta
 
-const atualizarPaleta = async (id, sabor, descricao, foto, preco) => {
-   const paleta = {
-      sabor,
-      descricao,
-      foto,
-      preco,
-   };
+const atualizarPaleta = async (id) => {
+  const paleta = {
+    sabor,
+    descricao,
+    foto,
+    preco,
+  };
 
-   const resposta = await fetch(`${baseUrl}/paletas/atualizar-paleta/${id}`, {
-      method: "PUT",
-      headers: {
-         "Content-type": "application/json"
+  const resposta = await fetch(`${baseUrl}/paletas/atualizar-paleta/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(paleta),
+  });
 
-      },
-      mode:"cors",
-      body: JSON.stringify(paleta),
-   });
+  const paletaAtualizada = await resposta.json();
 
-   const paletaAtualizada = await resposta.json();
-
-   console.log(paletaAtualizada);
-   return paletaAtualizada;
-
+  return paletaAtualizada;
 };
 
-// atualizarPaleta(5,"Brigadeiro", "teste","testefoto", "10.50");
+// função confirmação
 
+const confirmarExclusao = (id) => {
+  document.querySelector(".Confirmacao").innerHTML = ` <div>
+   <p> Tem certeza?</p>
+   <button onclick="exlcuirPaleta(${id})">Sim</button>
+   <button onclick="retornar()">Não</button>
+   </div>`;
+};
+
+// retornar para a página inicial
+const retornar = () => {
+  window.location.reload(true);
+};
 //Exclui paletas:
 
 const exlcuirPaleta = async (id) => {
-   const resposta = await fetch(`${baseUrl}/paletas/excluir-paleta/${id}`, {
-      method: "DELETE",
-      mode:"cors",
-   });
+  const resposta = await fetch(`${baseUrl}/paletas/excluir-paleta/${id}`, {
+    method: "DELETE",
+    mode: "cors",
+  });
 
-   console.log(paletaExcluida);
-
-   if(resposta.status === 204){
-      return "Paleta Excluída com sucesso"
-   } else {
-      return "Paleta não encontrada"
-   }
-
+  if (resposta.status === 204) {
+    window.location.reload(true);
+    return "Paleta Excluída com sucesso";
+  } else {
+    return "Paleta não encontrada";
+  }
 };
 
-exlcuirPaleta(14);
+//Manipulação do documento (HMTL);
 
+const imprimirTodasAsPaletas = async () => {
+  const paletas = await buscarTodasAsPaletas();
 
+  paletas.forEach(function (element) {
+    document.querySelector("#paletaList").insertAdjacentHTML(
+      "beforeend",
+      `<div class= "paletaListaItem">
+                      <div>
+                         <div class ="PaletaListaItem__sabor"> ${
+                           element.sabor
+                         } </div>
+                         <div class ="PaletaListaItem__preco"> R$ ${parseFloat(
+                           element.preco
+                         ).toFixed(2)} </div>
+                         <div class ="PaletaListaItem__descricao"> ${
+                           element.descricao
+                         } </div>
+                      </div>
+                      <img class ="PaletaListaItem__foto" src=${
+                        element.foto
+                      } alt=${element.sabor}/>
+                      <button onclick="formularioAtualizar(${
+                        element.id
+                      })">Editar</button>
+                      <button onclick="confirmarExclusao(${
+                        element.id
+                      })">Excluir</button>
+                 </div>`
+    );
+  });
+};
 
+imprimirTodasAsPaletas();
 
+//Botão
+// const ImprimirUmaPaletaPorId = async () => {
 
+//    document.getElementById("paletaPesquisada").innerHTML = "";
 
+//    const input = document.getElementById("inputIdPaleta");
+//    const id = input.value;
 
+//    const paleta = await buscarPaletasPorId(id);
+
+//    if (paleta === false) {
+//       const mensagemDeErro = document.createElement("p");
+//       mensagemDeErro.id = "mensagemDeErro";
+//       mensagemDeErro.classList.add("MensagemDeErro");
+//       mensagemDeErro.innerText = "Nenhuma paleta encontrada";
+
+//       document.getElementById("paletaPesquisada").appendChild(mensagemDeErro);
+//    } else {
+
+//    document.getElementById("paletaPesquisada").innerHTML =
+//    `
+//    <div class= "paletaListaItem">
+//                       <div>
+//                          <div class ="PaletaListaItem__sabor"> ${paleta.sabor} </div>
+//                          <div class ="PaletaListaItem__preco"> R$ ${parseFloat(paleta.preco).toFixed(2)} </div>
+//                          <div class ="PaletaListaItem__descricao"> ${paleta.descricao} </div>
+//                       </div>
+//                       <img class ="PaletaListaItem__foto" src=${paleta.foto} alt=${paleta.sabor}/>
+//                  </div>
+//    `;
+//    };
+
+// };
+
+const imprimirUmaPaletaPorId = async () => {
+  document.getElementById("paletaPesquisada").innerHTML = "";
+
+  const input = document.getElementById("inputIdPaleta");
+  const id = input.value;
+
+  const paleta = await buscarPaletasPorId(id);
+
+  if (paleta === false) {
+    const mensagemDeErro = document.createElement("p");
+    mensagemDeErro.id = "mensagemDeErro";
+    mensagemDeErro.classList.add("MensagemDeErro");
+    mensagemDeErro.innerText = "Nenhuma paleta encontrada";
+
+    document.getElementById("paletaPesquisada").appendChild(mensagemDeErro);
+  } else {
+    document.getElementById("paletaPesquisada").innerHTML = `
+       <div class="CartaoPaleta">
+         <div class="CartaoPaleta__infos">
+           <h4>${paleta.sabor}</h4>
+           <span>R$${paleta.preco.toFixed(2)}</span>
+           <p>${paleta.descricao}</p>
+         </div>
+         <img src="./${paleta.foto}" alt="Paleta sabor ${
+      paleta.sabor
+    }" class="CartaoPaleta__foto"/>
+       </div>
+     `;
+  }
+};
